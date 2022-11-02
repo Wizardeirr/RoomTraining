c9f7eaf965d4e6fd2ffbb534d49a9d5d89e6e621
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-11-02 23:08:12 +0300

    Fragment Content is DOne

--- app/src/main/java/com/volkankelleci/sharephotosbasicly/NutritionInsides.kt
+++ app/src/main/java/com/volkankelleci/sharephotosbasicly/NutritionInsides.kt
@@ -10,9 +10,7 @@ class NutritionInsides : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
