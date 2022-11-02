47c0362d302c137155784bfe8994f5ba26d1bf35
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2022-11-02 22:35:22 +0300

    Fixed some Class

--- app/src/main/java/com/volkankelleci/sharephotosbasicly/MainActivity.kt
+++ app/src/main/java/com/volkankelleci/sharephotosbasicly/MainActivity.kt
@@ -12,25 +12,11 @@ import com.google.firebase.ktx.app
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var auth:FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    auth= Firebase.auth
-    }
-    fun log(view: View){
-        val email=editText.text.toString()
-        val sifre=passwordText.text.toString()
-        auth.createUserWithEmailAndPassword(email,sifre).addOnCompleteListener { task->
 
-            if (task.isSuccessful){
-
-                val intent= Intent(this,RecyclerActivitiy::class.java)
-                startActivity(intent)
-                finish()
-            }
-        }.addOnFailureListener { exception->
-            Toast.makeText(applicationContext,exception.localizedMessage,Toast.LENGTH_LONG)
-        }
     }
+
 }
